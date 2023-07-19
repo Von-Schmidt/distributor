f6bbe927a6958039670795e1e7b995897355b4f8
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-07-19 17:15:26 +0200

    Added deletion and automatic group assignment

--- flask/restapi.py
+++ flask/restapi.py
@@ -10,15 +10,15 @@ def process():
     data = request.json
     string_input1 = data.get('string_input1')
     string_input2 = data.get('string_input2')
-    #string_input3 = data.get('string_input3')
+    string_input3 = int(data.get('string_input3'))
 
     password = passwordgenerator.generate_password(14)
     
-    #if string_input3 == 1:
-    main.create_user(string_input1, password, string_input2)
-        #main.add_user_to_group(string_input1)
-    #elif string_input3 == 0:
-    #    main.delete_user(string_input1)
+    if string_input3 == 1:
+        main.create_user(string_input1, password, string_input2)
+        main.add_user_to_group(string_input1, "porg")
+    elif string_input3 == 0:
+        main.delete_user(string_input1)
     
     return string_input1
 
