ffaf905c1ba2aa20e65451885ee38ee2b7b12c9c
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-07-19 17:10:14 +0200

    UI changes

---
+++ webserver/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import App from './App.svelte';
+</script>
+
+<svelte:head>
+    <title>DISTRIBUTOR</title>
+    <meta name="description" content="Management distributorů" />
+</svelte:head>
+
+<section>
+    <App />
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.4;
+    }
+</style>
---
+++ webserver/src/routes/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+	import distributor from '$lib/images/distributor.svg';
+</script>
+
+<header>
+    <div>
+        <a href="/">
+        <img src={distributor} alt="logo">
+        </a>
+    </div>
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 10vh;
+    }
+
+    div {
+        width: 40vh;
+    }
+    
+</style>
