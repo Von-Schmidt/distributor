23686e07cf492b98495b285b88e26bf9c8dfaa11
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-07-19 17:10:25 +0200

    Added form to interact with the flask API

---
+++ webserver/src/routes/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import './styles.css';
+
+    let name = "";
+    let email = "";
+    let yesno;
+
+    async function callAPI() {
+        const response = await fetch('http://localhost:5000/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            string_input1: name,
+            string_input2: email,
+            string_input3: yesno
+        })
+        });
+    }
+</script>
+
+<input type="text" placeholder="username" bind:value={name}>
+<input type="text" placeholder="email" bind:value={email}>
+<input type="text" placeholder="yesno" bind:value={yesno}>
+
+<input type="submit" value="submit" on:click={callAPI}>
