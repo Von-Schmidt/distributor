5ab35d475bd7f438310ac34e815dbadc0033b73d
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-07-24 16:33:10 +0200

    SQLite integration

--- flask/restapi.py
+++ flask/restapi.py
@@ -1,26 +1,36 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import main, passwordgenerator
+import main, passwordgenerator, namebase
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api', methods=['POST'])
 def process():
+    conn = namebase.create_connection()
+
     data = request.json
-    string_input1 = data.get('string_input1')
-    string_input2 = data.get('string_input2')
-    string_input3 = int(data.get('string_input3'))
+    name = data.get('string_input1')
+    email = data.get('string_input2')
+    username = data.get('string_input3')
+    company = data.get('string_input4')
 
     password = passwordgenerator.generate_password(14)
 
-    if string_input3 == 1:
-        main.create_user(string_input1, password, string_input2)
-        main.add_user_to_group(string_input1, "porg")
-    elif string_input3 == 0:
-        main.delete_user(string_input1)
+    main.create_user(username, password, email)
+    main.add_user_to_group(username, "porg")
     
-    return string_input1
+    namebase.add_user(name, username, email, company)
+
+    namebase.close_connection(conn)
+
+    return username
+
+@app.route('/api', methods=['GET'])
+def get_users():
+    users = namebase.get_all_users()
+    return jsonify(users)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
